83d13ec8557d9ce626ac7175a764dbc426490859
Victor Ho <drjone@gmail.com>
2018-11-26 12:49:46 +0800

    add custom script

---
+++ build-tools/build-memo.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+#./configure --prefix=/Users/Victor/Documents/CLionProjects/ffmpeg-install-test --extra-cflags=-I/Users/Victor/Documents/CLionProjects/ffmpeg-install-test/include --extra-ldflags=-L/Users/Victor/Documents/CLionProjects/ffmpeg-install-test/lib  --enable-libuavs2d --enable-gpl
